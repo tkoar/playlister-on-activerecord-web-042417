c1aa7211bf7512da975dfbce23f61061f5dc16d1
tkoar <terrancekoar@gmail.com>
2017-05-04 17:45:53 -0400

    Done.

--- db/migrate/01_create_songs.rb
+++ db/migrate/01_create_songs.rb
@@ -1,2 +1,13 @@
 class CreateSongs < ActiveRecord::Migration
+
+  def change
+    create_table :songs do |t|
+      t.string :name
+      t.string :artist
+      t.string :genre
+      t.integer :artist_id
+      t.integer :genre_id
+    end
+  end
+
 end
